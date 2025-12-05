e6349610dfe0fb65c1da818261b6b509c7974b26
Marje123456 <marjeiris.rodriguez@gmail.com>
2025-12-05 10:26:28 -0400

    ultimos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,125 +1,196 @@
-@extends('adminlte::auth.auth-page', ['auth_type' => 'register'])
+@extends('adminlte::page')
 
-@section('adminlte_css')
-    @parent
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+@section('title', 'Registrar Nuevo Usuario')
+
+@section('content_header')
+    <h1>Registrar Nuevo Usuario</h1>
 @stop
 
-@section('auth_header', __('adminlte::adminlte.register_message'))
-
-@section('auth_body')
-    <form action="{{ route('register') }}" method="post">
-        @csrf
-
-        {{-- Nombre --}}
-        <div class="input-group mb-3">
-            <input type="text" name="name" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
-                   value="{{ old('name') }}" placeholder="{{ __('Nombre') }}" autofocus>
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-user {{ config('adminlte.classes_auth_icon', '') }}"></span>
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('register') }}" method="post">
+                @csrf
+                
+                <div class="row">
+                    <div class="col-md-6">
+                        {{-- Nombre --}}
+                        <div class="form-group">
+                            <label for="name">Nombre</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="text" name="name" id="name" 
+                                       class="form-control @error('name') is-invalid @enderror" 
+                                       value="{{ old('name') }}" 
+                                       placeholder="Ingrese el nombre" required autofocus>
+                            </div>
+                            @error('name')
+                                <span class="invalid-feedback d-block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6">
+                        {{-- Apellido --}}
+                        <div class="form-group">
+                            <label for="last_name">Apellido</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="text" name="last_name" id="last_name" 
+                                       class="form-control @error('last_name') is-invalid @enderror" 
+                                       value="{{ old('last_name') }}" 
+                                       placeholder="Ingrese el apellido" required>
+                            </div>
+                            @error('last_name')
+                                <span class="invalid-feedback d-block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-            </div>
-            @if($errors->has('name'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('name') }}</strong>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        {{-- DNI --}}
+                        <div class="form-group">
+                            <label for="dni">DNI</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-id-card"></i></span>
+                                </div>
+                                <input type="text" name="dni" id="dni" 
+                                       class="form-control @error('dni') is-invalid @enderror" 
+                                       value="{{ old('dni') }}" 
+                                       placeholder="Ingrese el DNI" required>
+                            </div>
+                            @error('dni')
+                                <span class="invalid-feedback d-block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6">
+                        {{-- Email --}}
+                        <div class="form-group">
+                            <label for="email">Correo Electrónico</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                </div>
+                                <input type="email" name="email" id="email" 
+                                       class="form-control @error('email') is-invalid @enderror" 
+                                       value="{{ old('email') }}" 
+                                       placeholder="Ingrese el correo electrónico" required>
+                            </div>
+                            @error('email')
+                                <span class="invalid-feedback d-block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-            @endif
+
+                <div class="row">
+                    <div class="col-md-6">
+                        {{-- Contraseña --}}
+                        <div class="form-group">
+                            <label for="password">Contraseña</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                                </div>
+                                <input type="password" name="password" id="password" 
+                                       class="form-control @error('password') is-invalid @enderror" 
+                                       placeholder="Ingrese la contraseña" required>
+                            </div>
+                            @error('password')
+                                <span class="invalid-feedback d-block" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6">
+                        {{-- Confirmar Contraseña --}}
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirmar Contraseña</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                                </div>
+                                <input type="password" name="password_confirmation" id="password_confirmation" 
+                                       class="form-control" 
+                                       placeholder="Confirme la contraseña" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group mt-4">
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fas fa-user-plus"></i> Registrar Usuario
+                    </button>
+                    <a href="{{ route('login') }}" class="btn btn-secondary">
+                        <i class="fas fa-arrow-left"></i> Volver al Inicio de Sesión
+                    </a>
+                </div>
+            </form>
         </div>
-
-        {{-- Apellido --}}
-        <div class="input-group mb-3">
-            <input type="text" name="last_name" class="form-control {{ $errors->has('last_name') ? 'is-invalid' : '' }}"
-                   value="{{ old('last_name') }}" placeholder="{{ __('Apellido') }}">
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-user {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
-            </div>
-            @if($errors->has('last_name'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('last_name') }}</strong>
-                </div>
-            @endif
-        </div>
-
-        {{-- DNI --}}
-        <div class="input-group mb-3">
-            <input type="text" name="dni" class="form-control {{ $errors->has('dni') ? 'is-invalid' : '' }}"
-                   value="{{ old('dni') }}" placeholder="{{ __('DNI') }}">
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-id-card {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
-            </div>
-            @if($errors->has('dni'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('dni') }}</strong>
-                </div>
-            @endif
-        </div>
-
-        {{-- Email --}}
-        <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
-                   value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}">
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
-            </div>
-            @if($errors->has('email'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </div>
-            @endif
-        </div>
-
-        {{-- Contraseña --}}
-        <div class="input-group mb-3">
-            <input type="password" name="password"
-                   class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
-                   placeholder="{{ __('adminlte::adminlte.password') }}">
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
-            </div>
-            @if($errors->has('password'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </div>
-            @endif
-        </div>
-
-        {{-- Confirmar Contraseña --}}
-        <div class="input-group mb-3">
-            <input type="password" name="password_confirmation"
-                   class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}"
-                   placeholder="{{ __('adminlte::adminlte.retype_password') }}">
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
-            </div>
-            @if($errors->has('password_confirmation'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('password_confirmation') }}</strong>
-                </div>
-            @endif
-        </div>
-
-        <button type="submit" class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
-            <span class="fas fa-user-plus"></span>
-            {{ __('adminlte::adminlte.register') }}
-        </button>
-    </form>
+    </div>
 @stop
 
-@section('auth_footer')
-    <p class="my-0">
-        <a href="{{ route('login') }}">
-            {{ __('adminlte::adminlte.i_already_have_a_membership') }}
-        </a>
-    </p>
+@section('css')
+    <style>
+        .card {
+            box-shadow: 0 0 15px rgba(0,0,0,0.1);
+            border-radius: 10px;
+            border: none;
+        }
+        
+        .card-header {
+            background-color: #4e73df;
+            color: white;
+            border-radius: 10px 10px 0 0 !important;
+            padding: 1rem 1.25rem;
+        }
+        
+        .card-body {
+            padding: 2rem;
+        }
+        
+        .form-control {
+            border-radius: 5px;
+            border: 1px solid #d1d3e2;
+            padding: 0.5rem 0.75rem;
+            height: calc(2.25rem + 2px);
+        }
+        
+        .input-group-text {
+            background-color: #f8f9fc;
+            border: 1px solid #d1d3e2;
+            border-right: none;
+            border-radius: 5px 0 0 5px;
+        }
+        
+        .btn {
+            border-radius: 5px;
+            padding: 0.5rem 1.5rem;
+            font-weight: 600;
+        }
+        
+        .btn i {
+            margin-right: 0.5rem;
+        }
+    </style>
 @stop
